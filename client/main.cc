df188f1f343badf665d323badfb195911f202ac3
NoCodeNoLife-cloud <54143278+NoCodeNoLife-cloud@users.noreply.github.com>
2025-05-01 12:55:05 +0800

    ✨ feat(client/main): Add try-catch block to business logic for error handling

--- client/main.cc
+++ client/main.cc
@@ -1,15 +1,17 @@
 #include "aop/LauncherAspect.hpp"
 
+// ReSharper disable once CppDFAConstantFunctionResult
 auto business() -> bool {
-  // Implement business logic here.
-  return EXIT_SUCCESS;
+  try {
+    // Implement business logic here.
+    return EXIT_SUCCESS;
+  } catch (const std::exception& e) {
+    LOG(ERROR) << "Error: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 }
 
 auto main([[maybe_unused]] int32_t argc, [[maybe_unused]] char* argv[]) -> int32_t {
-  try {
-    framework::aop::LauncherAspect launcher;
-    launcher.exec(business);
-  } catch (const std::exception& e) {
-    LOG(ERROR) << "Error: " << e.what() << std::endl;
-  }
+  framework::aop::LauncherAspect launcher;
+  launcher.exec(business);
 }
