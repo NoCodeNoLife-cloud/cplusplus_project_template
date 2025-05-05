4408629cc8439b088c7a663fa0f1f2509d11433e
NoCodeNoLife-cloud <54143278+NoCodeNoLife-cloud@users.noreply.github.com>
2025-05-05 18:22:10 +0800

    ✨ feat(log): Configure GLog to only log to stderr

--- framework/log/GLogConfiguration.cc
+++ framework/log/GLogConfiguration.cc
@@ -1,7 +1,5 @@
-#include "GLogConfiguration.hpp"
-#include <iostream>
 #include <filesystem/Directory.hpp>
-#include "time/Clock.hpp"
+#include <log/GLogConfiguration.hpp>
 
 namespace framework::log {
   GLogConfiguration::GLogConfiguration() {
@@ -11,32 +9,14 @@ namespace framework::log {
   auto GLogConfiguration::config() const -> void {
     google::InitGoogleLogging(LOG_NAME.c_str());
     FLAGS_minloglevel = MIN_LOG_LEVEL;
-    configLogToFile();
     configLogToStdout();
     if (std::atexit(clean) != 0) {
       throw std::runtime_error("Failed to register cleanup function!");
     }
   }
 
-  auto GLogConfiguration::configLogToFile() const -> void {
-    const std::string now_time = time::Clock::getCompressedCurrentDateTimeString();
-    // ReSharper disable once CppDFAConstantConditions
-    if (const filesystem::Directory log_directory(LOG_FILE_DIRECTORY); log_directory.exists() && CLEAR_BEFORE_LOG && !log_directory.clearAll()) {
-      throw std::runtime_error("Could not remove log file!");
-    }
-    const std::string log_path = LOG_FILE_DIRECTORY + now_time + '/';
-    if (const filesystem::Directory dir{log_path}; !dir.exists() && !dir.mkdir()) {
-      throw std::runtime_error("Failed to create log file!");
-    }
-    google::SetLogDestination(google::GLOG_INFO, (log_path + "info_").c_str());
-    google::SetLogDestination(google::GLOG_WARNING, (log_path + "warning_").c_str());
-    google::SetLogDestination(google::GLOG_ERROR, (log_path + "error_").c_str());
-    google::SetLogDestination(google::GLOG_FATAL, (log_path + "fatal_").c_str());
-  }
-
-  auto GLogConfiguration::configLogToStdout() const -> void {
-    FLAGS_alsologtostderr = ALSO_LOG_TO_STDOUT;
-    FLAGS_colorlogtostderr = COLOR_LOG_TO_STDOUT;
+  auto GLogConfiguration::configLogToStdout() -> void {
+    FLAGS_logtostderr = true;
   }
 
   auto GLogConfiguration::clean() -> void {
--- framework/log/GLogConfiguration.hpp
+++ framework/log/GLogConfiguration.hpp
@@ -7,16 +7,10 @@ namespace framework::log {
     GLogConfiguration();
 
   private:
-    bool ALSO_LOG_TO_STDOUT{false};
-    bool COLOR_LOG_TO_STDOUT{false};
-    bool COLOR_LOG_TO_STDERR{true};
-    bool CLEAR_BEFORE_LOG{true};
     int32_t MIN_LOG_LEVEL{0};
     std::string LOG_NAME{"glog_main"};
-    std::string LOG_FILE_DIRECTORY{"../log/"};
     auto config() const -> void;
-    auto configLogToFile() const -> void;
-    auto configLogToStdout() const -> void;
+    static auto configLogToStdout() -> void;
     static auto clean() -> void;
   };
 } // namespace framework::log
