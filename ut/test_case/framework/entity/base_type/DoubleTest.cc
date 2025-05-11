7afd753a9ae9cbf00b36f26596b64a89996901ae
NoCodeNoLife-cloud <54143278+NoCodeNoLife-cloud@users.noreply.github.com>
2025-05-11 08:35:35 +0800

    ♻️ REFACT(DoubleTest): Add const to Double instances and fix assignment operator.

--- ut/test_case/framework/entity/base_type/DoubleTest.cc
+++ ut/test_case/framework/entity/base_type/DoubleTest.cc
@@ -5,25 +5,25 @@
 
 namespace gtest_case {
   TEST(DoubleTest, ConstructorTest) {
-    common::Double d1(2.5);
+    const common::Double d1(2.5);
     EXPECT_EQ(d1.doubleValue(), 2.5);
-    common::Double d2(common::Double::POSITIVE_INFINITY);
+    const common::Double d2(common::Double::POSITIVE_INFINITY);
     EXPECT_EQ(d2.doubleValue(), std::numeric_limits<double>::infinity());
-    common::Double d3(common::Double::NEGATIVE_INFINITY);
+    const common::Double d3(common::Double::NEGATIVE_INFINITY);
     EXPECT_EQ(d3.doubleValue(), -std::numeric_limits<double>::infinity());
-    common::Double d4(common::Double::NaN);
+    const common::Double d4(common::Double::NaN);
     EXPECT_TRUE(std::isnan(d4.doubleValue()));
   }
 
   TEST(DoubleTest, CopyConstructorTest) {
-    common::Double d1(3.14);
-    common::Double d2(d1);
+    const common::Double d1(3.14);
+    const common::Double d2(d1);
     EXPECT_EQ(d2.doubleValue(), 3.14);
   }
 
   TEST(DoubleTest, MoveConstructorTest) {
     common::Double d1(42.0);
-    common::Double d2(std::move(d1));
+    const common::Double d2(std::move(d1));
     EXPECT_EQ(d2.doubleValue(), 42.0);
   }
 
@@ -32,33 +32,34 @@ namespace gtest_case {
     common::Double d2(2.0);
     d2 = d1;
     EXPECT_EQ(d2.doubleValue(), 1.0);
+    // ReSharper disable once CppIdenticalOperandsInBinaryExpression
     d1 = d1;
     EXPECT_EQ(d1.doubleValue(), 1.0);
   }
 
   TEST(DoubleTest, ComparisonOperatorsTest) {
-    common::Double d1(5.0);
-    common::Double d2(5.0);
-    common::Double d3(6.0);
+    const common::Double d1(5.0);
+    const common::Double d2(5.0);
+    const common::Double d3(6.0);
     EXPECT_TRUE(d1 == d2);
     EXPECT_FALSE(d1 == d3);
     EXPECT_TRUE(d1 < d3);
     EXPECT_FALSE(d3 < d1);
-    common::Double d4(common::Double::NaN);
+    const common::Double d4(common::Double::NaN);
     EXPECT_FALSE(d4 == d4);
     EXPECT_FALSE(d4 < d4);
   }
 
   TEST(DoubleTest, ArithmeticOperatorsTest) {
-    common::Double d1(4.0);
-    common::Double d2(2.0);
+    const common::Double d1(4.0);
+    const common::Double d2(2.0);
     EXPECT_EQ((d1 + d2).doubleValue(), 6.0);
     EXPECT_EQ((d1 - d2).doubleValue(), 2.0);
     EXPECT_EQ((d1 * d2).doubleValue(), 8.0);
     EXPECT_EQ((d1 / d2).doubleValue(), 2.0);
-    common::Double zero(0.0);
+    const common::Double zero(0.0);
     EXPECT_THROW(d1 / zero, std::overflow_error);
-    common::Double nan(common::Double::NaN);
+    const common::Double nan(common::Double::NaN);
     EXPECT_TRUE(std::isnan((d1 + nan).doubleValue()));
   }
 
@@ -75,18 +76,18 @@ namespace gtest_case {
   }
 
   TEST(DoubleTest, ToStringTest) {
-    common::Double d1(3.14);
+    const common::Double d1(3.14);
     EXPECT_EQ(d1.toString(), "class common::Double{3.14}");
-    common::Double d2(common::Double::POSITIVE_INFINITY);
+    const common::Double d2(common::Double::POSITIVE_INFINITY);
     EXPECT_EQ(d2.toString(), "class common::Double{inf}");
-    common::Double d3(common::Double::NaN);
+    const common::Double d3(common::Double::NaN);
     EXPECT_EQ(d3.toString(), "class common::Double{nan}");
   }
 
   TEST(DoubleTest, HashCodeTest) {
-    common::Double d1(1.0);
-    common::Double d2(1.0);
-    common::Double d3(2.0);
+    const common::Double d1(1.0);
+    const common::Double d2(1.0);
+    const common::Double d3(2.0);
     EXPECT_EQ(d1.hashCode(), d2.hashCode());
     EXPECT_NE(d1.hashCode(), d3.hashCode());
   }
