4fc10cfb1469b3efec18cef922e5986b78e6e02e
Wolfgang von Goethe Johann <54143278+NoCodeNoLife-cloud@users.noreply.github.com>
2025-07-12 19:35:20 +0800

    feat(app/consoleApp): Add ConsoleExecutor class
    Added ConsoleExecutor class for console application execution.  Includes basic execution method stub.

---
+++ app/consoleApp/ConsoleExecutor.cc
@@ -0,0 +1,17 @@
+#include "ConsoleExecutor.hpp"
+
+namespace app
+{
+    auto ConsoleExecutor::execute(int32_t argc, char* argv[]) -> bool
+    {
+        try
+        {
+            // Please enter your execution code here
+        }
+        catch (...)
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ app/consoleApp/ConsoleExecutor.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "interface/IApplicationExecutor.hpp"
+
+namespace app {
+
+class ConsoleExecutor final :public IApplicationExecutor {
+public:
+    [[nodiscard]] auto execute(int32_t argc, char* argv[]) -> bool override;
+};
+
+}
