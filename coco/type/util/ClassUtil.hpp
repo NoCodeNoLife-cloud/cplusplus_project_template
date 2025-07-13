91672aaa1108ffa1302d5029fd426474143d3801
Wolfgang von Goethe Johann <54143278+NoCodeNoLife-cloud@users.noreply.github.com>
2025-07-13 19:14:08 +0800

    feat(coco/type/util): Add typeid retrieval functions
    Added new functions to ClassUtil for retrieving type IDs with and without CV-qualifiers, both by reference and by class.

--- coco/type/util/ClassUtil.hpp
+++ coco/type/util/ClassUtil.hpp
@@ -7,13 +7,41 @@ namespace coco
     {
     public:
         ClassUtil() = delete;
+
         template <typename T>
         static auto getTypeId(const T& t) -> std::string;
+
+        template <typename T>
+        static auto getTypeIdWithCvr(const T& t) -> std::string;
+
+        template <typename T>
+        static auto getTypeIdByClass() -> std::string;
+
+        template <typename T>
+        static auto getTypeIdWithCvrByClass(const std::string& t) -> std::string;
     };
 
     template <typename T>
-    auto ClassUtil::getTypeId([[maybe_unused]] const T& t) -> std::string
+    auto ClassUtil::getTypeId(const T&) -> std::string
     {
-        return boost::typeindex::type_id_with_cvr<decltype(t)>().pretty_name();
+        return boost::typeindex::type_id<T>().pretty_name();
+    }
+
+    template <typename T>
+    auto ClassUtil::getTypeIdWithCvr(const T&) -> std::string
+    {
+        return boost::typeindex::type_id_with_cvr<T>().pretty_name();
+    }
+
+    template <typename T>
+    auto ClassUtil::getTypeIdByClass() -> std::string
+    {
+        return boost::typeindex::type_id<T>().pretty_name();
+    }
+
+    template <typename T>
+    auto ClassUtil::getTypeIdWithCvrByClass(const std::string& t) -> std::string
+    {
+        return boost::typeindex::type_id_with_cvr<T>().pretty_name();
     }
 }
