d4dd4d9bf69a06e42895de3c47fe2e932b7dd0c3
Wolfgang von Goethe Johann <54143278+NoCodeNoLife-cloud@users.noreply.github.com>
2025-07-13 11:26:22 +0800

    Move: Move test files to correct location
    Refactor: Move test files from entity to type directory.  This better reflects the tested code's location.

--- coco/algorithm/InterpolationAlgorithm.hpp
+++ coco/algorithm/InterpolationAlgorithm.hpp
@@ -5,6 +5,7 @@ namespace coco
 {
     class InterpolationAlgorithm
     {
+    public:
         static auto linearInterpolation(const std::vector<std::pair<double, double>>& dataPoints, double x) -> double;
     };
 }
