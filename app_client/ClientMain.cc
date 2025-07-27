843a4489f506defea32b521627f1dcad54faac77
Wolfgang von Goethe Johann <54143278+NoCodeNoLife-cloud@users.noreply.github.com>
2025-07-28 01:47:54 +0800

    refactor(app_client): uncomment code and remove unused includes
    - Uncommented grpc channel arguments setup
    - Uncommented client creation and message send
    - Removed unused include for SystemInfo
    - Cleaned up commented-out code

--- app_client/ClientMain.cc
+++ app_client/ClientMain.cc
@@ -2,28 +2,22 @@
 
 #include "RpcClient.hpp"
 #include "aop/LauncherAspect.hpp"
-#include "utils/system/SystemInfo.hpp"
 
 auto mainTask() -> bool
 {
     // Setup channel.
-    // grpc::ChannelArguments channel_args;
-    // channel_args.SetInt(GRPC_ARG_KEEPALIVE_TIME_MS, 30000);
-    // channel_args.SetInt(GRPC_ARG_KEEPALIVE_TIMEOUT_MS, 5000);
-    // channel_args.SetInt(GRPC_ARG_KEEPALIVE_PERMIT_WITHOUT_CALLS, 1);
+    grpc::ChannelArguments channel_args;
+    channel_args.SetInt(GRPC_ARG_KEEPALIVE_TIME_MS, 30000);
+    channel_args.SetInt(GRPC_ARG_KEEPALIVE_TIMEOUT_MS, 5000);
+    channel_args.SetInt(GRPC_ARG_KEEPALIVE_PERMIT_WITHOUT_CALLS, 1);
 
     // Create client.
-    // const client_app::RpcClient client(grpc::CreateCustomChannel("localhost:50051", grpc::InsecureChannelCredentials(), channel_args));
+    const client_app::RpcClient client(grpc::CreateCustomChannel("localhost:50051", grpc::InsecureChannelCredentials(), channel_args));
 
     try
     {
         // Send a message.
-        // LOG(INFO) << client.Send("hello world");
-        auto item = common::SystemInfo::GetNetworkAdapters();
-        for (auto& i : item)
-        {
-            LOG(INFO) << i;
-        }
+        LOG(INFO) << client.Send("hello world");
     }
     catch (const std::exception& e)
     {
