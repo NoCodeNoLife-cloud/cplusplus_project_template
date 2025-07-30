399d95e9d39b1796abba71fc613f9e96d7a745e0
Wolfgang von Goethe Johann <54143278+NoCodeNoLife-cloud@users.noreply.github.com>
2025-07-30 21:12:07 +0800

    refactor(common): improve LauncherAspect and RpcServiceImpl
    - Update LauncherAspect:  - Add glog logging inclusion  - Remove unnecessary move semantics - Include PeriodicActuator for potential periodic tasks
    - Update RpcServiceImpl:  - Add input validation for Send method
      - Enhance logging with more details

--- app_server/rpc/RpcServiceImpl.cc
+++ app_server/rpc/RpcServiceImpl.cc
@@ -6,7 +6,14 @@ namespace server_app {
 grpc::Status RpcServiceImpl::Send(grpc::ServerContext* context,
                                   const rpc::MessageRequest* request,
                                   rpc::MessageResponse* response) {
-  LOG(INFO) << "Received message: " << request->content();
+  if (!context || !request || !response) {
+    LOG(ERROR) << "Invalid input parameters: context=" << context
+               << ", request=" << request << ", response=" << response;
+    return {grpc::StatusCode::INVALID_ARGUMENT, "Invalid input parameters"};
+  }
+
+  LOG(INFO) << "Received message from client: " << request->content()
+            << " (peer=" << context->peer() << ")";
   response->set_status("Message received successfully");
   return grpc::Status::OK;
 }
--- common/aop/LauncherAspect.cc
+++ common/aop/LauncherAspect.cc
@@ -1,7 +1,9 @@
 #include "LauncherAspect.hpp"
 
+#include <glog/logging.h>
+
 #include "log/GLogConfigurator.hpp"
-#include "type/util/ObjectFactory.hpp"
+#include "utils/thread/PeriodicActuator.hpp"
 
 namespace common {
 LauncherAspect::LauncherAspect() : timer_("program", true) {
@@ -12,8 +14,7 @@ LauncherAspect::LauncherAspect() : timer_("program", true) {
 auto LauncherAspect::onExit() -> void { timer_.recordEnd(true); }
 
 auto LauncherAspect::registerTask() -> void {
-  startup_tasks_.push_back(
-      std::move(std::make_unique<service::GLogConfigurator>()));
+  startup_tasks_.push_back(std::make_unique<service::GLogConfigurator>());
 }
 
 auto LauncherAspect::runAll() const -> void {
