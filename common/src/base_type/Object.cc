1bd664e1c4b76e9583d0dc63292e7c2d51e752b1
Wolfgang von Goethe Johann <54143278+NoCodeNoLife-cloud@users.noreply.github.com>
2026-01-07 22:21:09 +0800

    feat(common): enhance Object base class with utility methods
    - Added src directory to header file paths in CMakeLists.txt
    - Implemented toString() method to return class name and hash code
    - Added clone() method as virtual copy constructor with default exception
    - Implemented instanceOf() template method for type checking
    - Added isInstance() method for runtime type checking with type_info
    - Created getClassName() method to retrieve class name as string
    - Implemented is() method for reference equality comparison
    - Updated Object class documentation to reflect Java-like utilities
    - Added memory and string includes to Object class

--- common/src/base_type/Object.cc
+++ common/src/base_type/Object.cc
@@ -2,6 +2,8 @@
 
 #include <format>
 #include <typeinfo>
+#include <memory>
+#include <string>
 
 namespace common
 {
@@ -21,5 +23,37 @@ namespace common
         -> size_t
     {
         return std::hash<const void*>{}(this);
+    }
+
+    auto Object::toString() const
+        -> std::string
+    {
+        return std::string{getClass().name()} + "@" + std::to_string(hashCode());
+    }
+
+    auto Object::clone() const
+        -> std::unique_ptr<Object>
+    {
+        // Since Object is likely to be used as a base for polymorphic types, 
+        // we throw an exception to indicate that derived classes should implement this.
+        throw std::logic_error("clone() must be implemented by derived classes");
+    }
+
+    auto Object::isInstance(const std::type_info& target_type) const noexcept
+        -> bool
+    {
+        return target_type == typeid(*this);
+    }
+
+    auto Object::getClassName() const
+        -> std::string
+    {
+        return std::string{getClass().name()};
+    }
+
+    auto Object::is(const Object& other) const noexcept
+        -> bool
+    {
+        return this == &other;
     }
 }
--- common/src/base_type/Object.hpp
+++ common/src/base_type/Object.hpp
@@ -1,11 +1,14 @@
 #pragma once
 #include <format>
 #include <typeinfo>
+#include <memory>
+#include <string>
 
 namespace common
 {
     /// @brief Base class for all objects in the system.
-    /// @details This class provides basic functionality for type information, hashing, and string representation.
+    /// @details This class provides basic functionality for type information, hashing, string representation,
+    /// and object utilities similar to Java's Object class.
     /// It is designed to be inherited by other classes that need these features.
     // ReSharper disable once CppClassCanBeFinal
     class Object
@@ -19,7 +22,7 @@ namespace common
             -> const std::type_info&;
 
         /// @brief Compares this object with another object for equality.
-        /// @param other The object to compare with.
+        /// @param[in] other The object to compare with.
         /// @return true if the objects are equal, false otherwise.
         /// @details By default, this implementation compares the memory addresses of the objects.
         /// Derived classes should override this method to provide meaningful equality comparison.
@@ -38,7 +41,42 @@ namespace common
         /// @details This method should be overridden by derived classes to provide a meaningful string
         /// representation of the object's content. The default implementation returns the type name.
         [[nodiscard]] virtual auto toString() const
-            -> std::string = 0;
+            -> std::string;
+
+        /// @brief Creates a copy of this object.
+        /// @return std::unique_ptr<Object> A new instance that is a copy of this object.
+        /// @details This is a virtual copy constructor. Derived classes should override this method
+        /// to provide proper copying behavior. The default implementation throws an exception
+        /// to indicate that derived classes should implement this.
+        [[nodiscard]] virtual auto clone() const
+            -> std::unique_ptr<Object>;
+
+        /// @brief Checks if this object is an instance of the specified type.
+        /// @tparam T The type to check against.
+        /// @return true if the object is an instance of type T, false otherwise.
+        template<typename T>
+        [[nodiscard]] auto instanceOf() const noexcept
+            -> bool
+        {
+            return dynamic_cast<const T*>(this) != nullptr;
+        }
+
+        /// @brief Checks if this object is an instance of the specified type at runtime.
+        /// @param[in] target_type The type_info of the target type to check against.
+        /// @return true if the object is an instance of the specified type, false otherwise.
+        [[nodiscard]] auto isInstance(const std::type_info& target_type) const noexcept
+            -> bool;
+
+        /// @brief Gets the class name as a string.
+        /// @return std::string The name of the class.
+        [[nodiscard]] auto getClassName() const
+            -> std::string;
+
+        /// @brief Checks if this object is the same as another object (reference equality).
+        /// @param[in] other The object to compare references with.
+        /// @return true if both objects are the same reference, false otherwise.
+        [[nodiscard]] auto is(const Object& other) const noexcept
+            -> bool;
 
     private:
         friend std::formatter<Object>;
